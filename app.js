04ab231f545771fbc48cd6f0d6e81d2d4173aa21
unknown <icoul35@gmail.com>
2020-05-27 16:14:29 +0900

    update folder name 'route' to 'server

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var infoRouter = require('./routes/info');
+var indexRouter = require('./servers/index');
+var usersRouter = require('./servers/users');
+var infoRouter = require('./servers/info');
 
 var app = express();
 
