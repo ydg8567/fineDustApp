4dc9558e0d07fb26d91875927aec5b2caef6b6c4
unknown <icoul35@gmail.com>
2020-05-27 16:12:30 +0900

    apply data from backend to main page and chart

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  console.log(moment().format('YYYY년 MM월 DD일 hh시'));
+  
+  res.render('index', {today: moment().format('YYYY년 MM월 DD일 hh시'),
+                       dustHour: 33,
+                       dustDay: 31,
+                       UltrafineHour: 13,
+                       UltrafineDay: 11,
+                       data: 30, 
+                       lineGraphData: [
+                                        {'rgstDt': '2020-05-25 14:11:00', 'value': 25},
+                                        {'rgstDt': '2020-05-25 14:12:00', 'value': 35},
+                                        {'rgstDt': '2020-05-25 14:13:00', 'value': 45},
+                                        {'rgstDt': '2020-05-25 14:14:00', 'value': 35},
+                                        {'rgstDt': '2020-05-25 14:15:00', 'value': 25},
+                                        {'rgstDt': '2020-05-25 14:16:00', 'value': 35},
+                                        {'rgstDt': '2020-05-25 14:17:00', 'value': 45},
+                                        {'rgstDt': '2020-05-25 14:18:00', 'value': 35},
+                                        {'rgstDt': '2020-05-25 14:19:00', 'value': 25},
+                                        {'rgstDt': '2020-05-25 14:20:00', 'value': 35},
+                                      ]});
 });
 
 module.exports = router;
