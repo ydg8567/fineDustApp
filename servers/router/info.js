418315c21a1116778a4b285326fe1753d0825dd2
unknown <icoul35@gmail.com>
2020-05-31 17:06:44 +0900

    add function for get the dust data by Ajax

--- servers/router/info.js
+++ servers/router/info.js
@@ -17,4 +17,32 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.get('/api/search', (req, res, next) => {
+  const query = `SELECT * FROM gas_log_tb WHERE module_idx = 2 LIMIT 30`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (!err) {
+      let result;
+
+      rows.forEach(data => {
+        const html = `
+          <tr>
+            <td>${data.log_idx}</td>
+            <td>${data.A1}</td>
+            <td>${data.temperature}</td>
+            <td>${data.freqeuncy}</td>
+            <td>${data.rgst_dt}</td>
+          </tr>
+        `
+        result += html;
+      });
+      res.send(result);
+    }
+    else {
+      console.log(err);
+      res.send(err);
+    }
+  })
+})
+
 module.exports = router;
